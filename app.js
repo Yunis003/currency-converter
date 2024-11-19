54131d33018955710f3cda266d26893b8bf03582
Yunis003 <hi.yunisqulamov@gmail.com>
2024-11-20 01:02:45 +0400

    created_changing_structure

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ currencyFrom.forEach((element)=>{
         document.querySelector('.purpleBack1').classList.remove("purpleBack1");
         element.classList.add("purpleBack1");
         obj.from = element.textContent;
-        console.log(obj.from)
-        currentValue1.textContent = '1' + ' ' + obj.from + ' ' + '=' + `` + obj.to;
-        // fetchingData();
+        fetchingData();
     })
 })
 currencyTo.forEach((element)=>{
@@ -33,22 +31,104 @@ currencyTo.forEach((element)=>{
         document.querySelector('.purpleBack2').classList.remove("purpleBack2");
         element.classList.add("purpleBack2");
         obj.to = element.textContent;
-        console.log(obj.to);
-        currentValue2.textContent = '1' + ' ' + obj.to + ' ' + '=' + `` + obj.from;
-        // fetchingData();
+        fetchingData();
     })
 })
 
+async function fetchingData(){
+    await fetch(`https://v6.exchangerate-api.com/v6/3195f2d508e35437dd7db01f/latest/${obj.from}`)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        let conversionRateTo = data.conversion_rates[obj.to];
+        let conversionRateFrom = data.conversion_rates[obj.from];
+        obj.currentFrom = conversionRateFrom;
+        obj.currentTo = conversionRateTo;
+        currentValue1.textContent = `1 ${obj.from} = ${conversionRateTo} ${obj.to}`;
+        currentValue2.textContent = `1 ${obj.to} = ${(1 / conversionRateTo).toFixed(4)} ${obj.from}`;
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
+// ele bir sey elemelisen ki tutax ki bu currentvalue 1 ve 2 deyisir ama sinxronda deyismeliyem cunki beyaq ayrica curencto nun icinde eledim deyismir dinamik gerey 2 defe sorusum oda sefdi
+//! update the values dynamically
+
+inputFrom.addEventListener("input", ()=>{
+    let reNewed = inputFrom.value.replace(/[^0-9.,]/g, '').replace(',', '.');
+    inputFrom.value = reNewed;
+    if (inputFrom.value.includes('.')){
+        let editedValue = inputFrom.value.split('.');
+        editedValue.splice(1, 0, '.');
+        editedValue = editedValue.join('');
+        inputTo.value = Number(editedValue) * obj.currentTo;
+    }
+    else if(inputFrom.value == ''){
+        inputTo.value = '';
+    }
+    else{
+        inputTo.value = inputFrom.value * obj.currentFrom;
+    }
+})
+inputTo.addEventListener("input", ()=>{
+    let reNewed2 = inputTo.value.replace(/[^0-9.,]/g, '').replace(',', '.');
+    inputTo.value = reNewed2;
+    
+    if (inputTo.value.includes(".")){
+        let editedValue = inputTo.value.split('.');
+        editedValue.splice(1, 0, '.');
+        editedValue = editedValue.join('');
+        inputFrom.value = Number(editedValue) * obj.currentFrom;
+    }
+    else if(inputTo.value == ''){
+        inputFrom.value = '';
+    }
+    else{
+        inputFrom.value = inputTo.value * obj.currentTo;
+    }
+})
+
+//* big o notationu daha cox olan bir metod
+// const obj = {
+//     from: 'RUB',
+//     to: 'USD',
+//     currentFrom: 0,
+//     currentTo: 0
+// }
+
+// currencyFrom.forEach((element)=>{
+//     element.addEventListener("click", ()=>{
+//         document.querySelector('.purpleBack1').classList.remove("purpleBack1");
+//         element.classList.add("purpleBack1");
+//         obj.from = element.textContent;
+//         fetchingData();
+//         console.log(obj.from)
+//         currentValue1.textContent = '1' + ' ' + obj.from + ' ' + '=' + `` + obj.to;
+//     })
+// })
+// currencyTo.forEach((element)=>{
+//     element.addEventListener("click", ()=>{
+//         document.querySelector('.purpleBack2').classList.remove("purpleBack2");
+//         element.classList.add("purpleBack2");
+//         obj.to = element.textContent;
+//         fetchingData();
+//         console.log(obj.to);
+//         currentValue2.textContent = '1' + ' ' + obj.to + ' ' + '=' + `` + obj.from;
+//     })
+// })
+
 // async function fetchingData(){
-//     await fetch(`https://v6.exchangerate-api.com/v6/3195f2d/latest/${obj.from}/${obj.to}`)
+//     await fetch(`https://v6.exchangerate-api.com/v6/3195f2d508e35437dd7db01f/latest/${obj.from}`)
 //     .then((response)=>{
 //         return response.json();
 //     })
 //     .then((data)=>{
 //         console.log(data);
 //         let keys = Object.keys(data.conversion_rates);
-//         obj.currentFrom = keys[0];
-//         obj.currentTo = keys[1];
+//         let values = Object.values(data.conversion_rates);
+//         obj.currentFrom = values[keys.indexOf(obj.from)];
+//         obj.currentTo = values[keys.indexOf(obj.to)];
 //         console.log(obj.currentFrom);
 //         console.log(obj.currentTo);
 //     })
@@ -56,43 +136,3 @@ currencyTo.forEach((element)=>{
 //         console.log(error);
 //     })
 // }
-
-//! update the values
-inputFrom.addEventListener("input", (event)=>{
-    let reNewed = inputFrom.value.replace(/[^0-9.]/g, '');  
-    let reNewed2 = reNewed.textContent.replace(',', '.');
-    inputFrom.value = reNewed2;   
-})
-
-
-// inputFrom.addEventListener("input", ()=>{
-//     console.log(inputFrom.value);
-//     if (inputFrom.value.includes('.')){
-//         let editedValue = inputFrom.value.split('.'); // eynisini seple vergul ucun sora vergulu evezle noqteynen metod avriydi adi yadimnan cixdi
-//         editedValue.splice(1, 0, '.');
-//         editedValue = editedValue.join('');
-//         console.log(editedValue);
-//         inputTo.value = Number(editedValue) * 0.0135;
-//     }
-//     else if(inputFrom.value == ''){
-//         inputTo.value = '';
-//     }
-//     else{
-//         inputTo.value = inputFrom.value * 0.0135;
-//     }
-// })
-inputTo.addEventListener("input", ()=>{
-    console.log(inputTo.value);
-    if (inputTo.value.includes(".")){
-        let editedValue = inputTo.value.split('.');
-        editedValue.splice(1, 0, '.');
-        editedValue = editedValue.join('');
-        inputFrom.value = Number(editedValue) * 73.8896;
-    }
-    else if(inputTo.value == ''){
-        inputFrom.value = '';
-    }
-    else{
-        inputFrom.value = inputTo.value / 73.8896;
-    }
-})
